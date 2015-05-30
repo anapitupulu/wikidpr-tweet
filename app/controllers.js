74ec45bb60df6ecd95ddc6510299f6dbfead9296
sleigner <alexnico7@gmail.com>
2015-05-30 20:45:21 +0000

    basic functionality works

--- app/controllers.js
+++ app/controllers.js
@@ -1,5 +1,84 @@
 //inject the twitterService into the controller
 angular.module('myApp')
 
-.controller('WikiDprTweetController', ['$scope', '$q', 'twitterService', function($scope, $q, twitterService) {
+.controller('WikiDprTweetController', ['$scope', '$q', 'twitterService', '$http', function($scope, $q, twitterService, $http) {
+
+    twitterService.initialize();
+
+    $scope.tweetText = '';
+    $scope.attr = {
+        userLoggedIn: twitterService.isReady(),
+        username: "",
+        latestTweets: [],
+        tweetTextLimitStyle: ""
+    };
+
+    var initializeLoggedIn = function() {
+        twitterService.getUserInfo().then(function (userInfo) {
+            $scope.attr.username = userInfo.screen_name;
+        });
+
+        twitterService.getUserLatestTweets().then(function (latestTweets) {
+            $scope.attr.latestTweets = latestTweets;
+        });
+    }
+
+    if ($scope.attr.userLoggedIn) {
+        initializeLoggedIn();
+    }
+
+    //when the user clicks the connect twitter button, the popup authorization window opens
+    $scope.connectButton = function() {
+        twitterService.connectTwitter().then(function() {
+            if (twitterService.isReady()) {
+                //if the authorization is successful, hide the connect button and display the tweets
+                $scope.attr.userLoggedIn = true;
+                initializeLoggedIn();
+            }
+        });
+    };
+
+    $scope.signOut = function() {
+        twitterService.clearCache();
+        $scope.attr.userLoggedIn = false;
+        $scope.attr.username = "";
+    };
+
+    $scope.findAnggota = function(anggota) {
+        var searchUrl = 'http://wikidpr.org/api/v2/anggota';
+        return $http.get(searchUrl, {
+            params: { s: anggota }
+        }).then(function (response) {
+            return response.data.data;
+        });
+    };
+
+    $scope.onAnggotaSelected = function($item, $model, $label) {
+       var tokenizedTwitterUrl = $item.twitter.split("/");
+       var twitterId = tokenizedTwitterUrl[tokenizedTwitterUrl.length-1];
+       var komisi = $item.komisi;
+       var partai = $item.partai
+
+       $scope.tweetText = "@" + twitterId + " " + "#" + partai + " #Komisi" + komisi + " ";
+    };
+
+    $scope.submitTweet = function(tweetText) {
+        twitterService.postTweet(tweetText).then(function() {
+            twitterService.getUserLatestTweets().then(function (latestTweets) {
+                $scope.attr.latestTweets = latestTweets;
+                $scope.anggotaSelected = "";
+                $scope.tweetText = "";
+            });
+        });
+    }
+
+    $scope.isTextLimitHit = function() {
+        if ($scope.tweetText.length > 180) {
+            $scope.attr.tweetTextLimitStyle = { "color": "red" };
+            return true;
+        } else {
+            $scope.attr.tweetTextLimitStyle = "";
+            return false;
+        }
+    }
 }]);
--- app/services.js
+++ app/services.js
@@ -1,41 +1,64 @@
-angular.module('myApp').factory('twitterService', function($q) {
+angular.module('myApp')
 
-    var authorizationResult = false;
+.factory('twitterService', function($q) {
+
+    var oauthHandle = null;
+    var connected = false; 
 
     return {
         initialize: function() {
             //initialize OAuth.io with public key of the application
             OAuth.initialize('7fJaEiehtRcguYmajy-uxDekk8U', {cache:true});
             //try to create an authorization result when the page loads, this means a returning user won't have to click the twitter button again
-            authorizationResult = OAuth.create('twitter');
+            oauthHandle = OAuth.create('twitter');
+            if (oauthHandle !== false) {
+                connected = true;
+            }
         },
         isReady: function() {
-            return (authorizationResult);
+            return (connected);
         },
         connectTwitter: function() {
             var deferred = $q.defer();
-            OAuth.popup('twitter', {cache:true}, function(error, result) { //cache means to execute the callback if the tokens are already present
-                if (!error) {
-                    authorizationResult = result;
-                    deferred.resolve();
-                } else {
-                    //do something if there's an error
-                }
+            OAuth.popup('twitter', {cache:true}).done(function(twitter) { //cache means to execute the callback if the tokens are already present
+                connected = true;
+                deferred.resolve();
+            }).fail(function (error) {
+                console.log(error);
             });
             return deferred.promise;
         },
         clearCache: function() {
             OAuth.clearCache('twitter');
-            authorizationResult = false;
+            connected = false;
         },
-        getLatestTweets: function () {
+        getUserLatestTweets: function () {
             //create a deferred object using Angular's $q service
             var deferred = $q.defer();
-            var promise = authorizationResult.get('/1.1/statuses/home_timeline.json').done(function(data) { //https://dev.twitter.com/docs/api/1.1/get/statuses/home_timeline
+            var promise = oauthHandle.get('/1.1/statuses/user_timeline.json?count=3').done(function(data) { //https://dev.twitter.com/docs/api/1.1/get/statuses/home_timeline
                 //when the data is retrieved resolved the deferred object
                 deferred.resolve(data)
             });
             //return the promise of the deferred object
+            return deferred.promise;
+        },
+        getUserInfo: function() {
+            var deferred = $q.defer();
+            var promise = oauthHandle.get('1.1/account/verify_credentials.json').done(function(data) {
+                deferred.resolve(data)
+            });
+
+            return deferred.promise;
+        },
+        postTweet: function(tweetText) {
+            var deferred = $q.defer();
+            var promise = oauthHandle.post('1.1/statuses/update.json', {
+                data: {
+                    status: tweetText
+                }
+            }).done(function(data) {
+                deferred.resolve();
+            });
             return deferred.promise;
         }
     }
