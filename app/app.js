0f52751e7143ded2320de0217c14e6ad27cbdeed
sleigner <alexnico7@gmail.com>
2015-06-01 02:47:53 +0000

    add twitter information in the typeahead drop down

--- app/app.js
+++ app/app.js
@@ -7,26 +7,15 @@ var app = angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
-
-app.controller('homeController', ['$scope', '$http', function ($scope, $http) {
-
-    $scope.komisi = [ 'Komisi I', 'Komisi II', 'Komisi III', 'Komisi IV' ];
-    $scope.selectedKomisi = $scope.komisi[0];
-
-    $scope.anggota = [
-        { id: 1, name: 'Alex', party: 'Libertarian', district: 'Malang'},
-        { id: 2, name: 'Indah', party: 'Democrat', district: 'Jambi' },
-        { id: 3, name: 'Zee', party: 'Green', district: 'Bukit Tinggi'},
-        { id: 4, name: 'Greg', party: 'Republican', district: 'Jakarta'}
-    ];
-    $scope.anggotaSelected = "";
-    $scope.tweetText = '';
-
-    $scope.$watch('anggotaSelected', function(newValue, oldValue) {
-        if (newValue.name !== undefined && newValue.id != oldValue.id) {
-            $scope.tweetText = '#' + newValue.name + ' #' + newValue.party + ' #' + newValue.district + ' ';
+}]).
+filter('extractTwitterId', function() {
+    return function(twitterUrl) {
+        twitterUrl = twitterUrl || '';
+        if (twitterUrl.length == 0) {
+            return '<span style="font-style: italic">twitter not found</span>';
         }
-    });
-
-}]);
+        var tokenizedTwitterUrl = twitterUrl.split("/");
+        var twitterId = tokenizedTwitterUrl[tokenizedTwitterUrl.length-1];
+        return "@" + twitterId;
+    };
+});
